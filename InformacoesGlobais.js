bd6fa72259cdc158ee9636f9f77de805286452a2
Tauane Lemke <111572842+tauuzinhaa@users.noreply.github.com>
2024-10-16 21:23:54 -0300

    1

--- InformacoesGlobais.js
+++ InformacoesGlobais.js
@@ -3,11 +3,15 @@ const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = (dados.tempo_medio - horas) * 100
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = 'Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
-    console.log(paragrafo);
+
+    paragrafo.innerHTML = 'Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas}bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}horas </span> e ${minutos} minutos conectadas.'
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
